a764b81f07cb0240a0a269799693f145bc3c1f78
Joseph Sola <jvsola@gmail.com>
2020-01-29 22:46:36 -0800

    Updates

---
+++ precache-manifest.4fd12ea76ef5d44b28d5d1a9549c93bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f360521c69eff2e904a9e6e341a47f96",
+    "url": "/poe-theory-craft/index.html"
+  },
+  {
+    "revision": "4fa83271cef29844d6e3",
+    "url": "/poe-theory-craft/static/css/main.67d3eecc.chunk.css"
+  },
+  {
+    "revision": "8c45a22e437db330547d",
+    "url": "/poe-theory-craft/static/js/2.c731919d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/poe-theory-craft/static/js/2.c731919d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4fa83271cef29844d6e3",
+    "url": "/poe-theory-craft/static/js/main.9be4735f.chunk.js"
+  },
+  {
+    "revision": "c966b89adbb5b2914886",
+    "url": "/poe-theory-craft/static/js/runtime-main.49a4fc52.js"
+  }
+]);
