b3db359258c0401b36bc69765b0dfe09dd13338a
Joseph Sola <jvsola@gmail.com>
2020-01-30 00:27:44 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/poe-theory-craft/precache-manifest.4fd12ea76ef5d44b28d5d1a9549c93bd.js"
+  "/poe-theory-craft/precache-manifest.aa2cafe71ad93a5dd9e0268703149782.js"
 );
 
 self.addEventListener('message', (event) => {
